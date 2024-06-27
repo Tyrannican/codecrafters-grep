d0ac0d509fa76d32e993b6ef3bf09730e4cc941f
Graham Keenan <graham.keenan@outlook.com>
2024-06-27 19:33:21 +0100

    postive character groups

---
+++ src/engine.rs
@@ -0,0 +1,113 @@
+#[derive(Debug, Clone)]
+pub enum RegexOperation {
+    PositiveCharGroup { chars: Vec<char> },
+    Digit,
+    Alphanumeric,
+    Literal { exact: String },
+}
+
+pub struct RegexEngine {
+    pub input: String,
+    pub operations: Vec<RegexOperation>,
+    matches: Vec<bool>,
+}
+
+impl RegexEngine {
+    pub fn new(input: String, pattern: String) -> Self {
+        Self {
+            input,
+            operations: parse_pattern(&pattern),
+            matches: vec![],
+        }
+    }
+
+    pub fn matches(&mut self) {
+        // TODO: Refactor
+        for op in self.operations.clone() {
+            match op {
+                RegexOperation::Literal { exact } => self.literal(&exact),
+                RegexOperation::Digit => self.digit(),
+                RegexOperation::Alphanumeric => self.alphanumeric(),
+                RegexOperation::PositiveCharGroup { chars } => self.positive_character_group(chars),
+            }
+        }
+
+        if self.matches.is_empty() || !self.matches.iter().all(|&m| m) {
+            std::process::exit(1);
+        }
+    }
+
+    fn literal(&mut self, pattern: &str) {
+        self.matches.push(self.input.contains(pattern));
+    }
+
+    fn digit(&mut self) {
+        for ch in self.input.chars() {
+            if ch.is_numeric() {
+                self.matches.push(true);
+                break;
+            }
+        }
+    }
+
+    fn alphanumeric(&mut self) {
+        for ch in self.input.chars() {
+            if ch.is_alphanumeric() {
+                self.matches.push(true);
+                break;
+            }
+        }
+    }
+
+    fn positive_character_group(&mut self, chars: Vec<char>) {
+        let mut matches = vec![];
+        for char in self.input.chars() {
+            if chars.contains(&char) {
+                matches.push(true);
+            }
+        }
+
+        if matches.is_empty() {
+            matches.push(false);
+        }
+        let result = matches.into_iter().all(|c| c);
+        self.matches.push(result);
+    }
+}
+
+fn parse_pattern(pattern: &str) -> Vec<RegexOperation> {
+    let mut ops = vec![];
+    let mut buf = String::new();
+    let mut iter = pattern.chars();
+
+    while let Some(next) = iter.next() {
+        match next {
+            '[' => {
+                let mut pat = Vec::new();
+                while let Some(ch) = iter.next() {
+                    if ch == ']' {
+                        break;
+                    }
+                    pat.push(ch);
+                }
+
+                ops.push(RegexOperation::PositiveCharGroup { chars: pat });
+            }
+            '\\' => {
+                let param = iter.next().unwrap();
+                match param {
+                    'd' => ops.push(RegexOperation::Digit),
+                    'w' => ops.push(RegexOperation::Alphanumeric),
+                    _ => unimplemented!("{param} not implemented"),
+                }
+            }
+            _ => buf.push(next),
+        }
+    }
+
+    if !buf.is_empty() {
+        ops.push(RegexOperation::Literal { exact: buf });
+    }
+
+    ops
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,21 @@
 use clap::Parser;
 use std::io;
 
+mod engine;
+use engine::RegexEngine;
+
 #[derive(Parser, Debug, Clone)]
 struct Cli {
     #[clap(short = 'E')]
     pattern: String,
 }
 
-fn matches_pattern(input: &str, pattern: String) -> bool {
-    match pattern.as_str() {
-        "\\d" => {
-            for char in input.chars() {
-                if char.is_numeric() {
-                    return true;
-                }
-            }
-
-            false
-        }
-        "\\w" => {
-            for char in input.chars() {
-                if char.is_alphanumeric() {
-                    return true;
-                }
-            }
-
-            false
-        }
-        _ => return input.contains(&pattern),
-    }
-}
-
 fn main() {
     let cli = Cli::parse();
     let pattern = cli.pattern;
     let mut input_line = String::new();
-    println!("Pattern: {pattern}");
-
     io::stdin().read_line(&mut input_line).unwrap();
-    if !matches_pattern(&input_line, pattern) {
-        std::process::exit(1);
-    }
+
+    let mut engine = RegexEngine::new(input_line, pattern);
+    engine.matches();
 }
