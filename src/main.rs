91164bf17bf92de9efefc305a84218836017b8a2
Graham Keenan <graham.keenan@outlook.com>
2024-06-27 17:58:42 +0100

    match literal

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,24 @@
-use std::env;
+use clap::Parser;
 use std::io;
-use std::process;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
-    }
+#[derive(Parser, Debug, Clone)]
+struct Cli {
+    #[clap(short = 'E')]
+    pattern: String,
 }
 
-// Usage: echo <input_text> | your_grep.sh -E <pattern>
+fn matches_pattern(input: &str, pattern: String) -> bool {
+    input.contains(&pattern)
+}
+
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    if env::args().nth(1).unwrap() != "-E" {
-        println!("Expected first argument to be '-E'");
-        process::exit(1);
-    }
-
-    let pattern = env::args().nth(2).unwrap();
+    let cli = Cli::parse();
+    let pattern = cli.pattern;
     let mut input_line = String::new();
+    println!("Pattern: {pattern}");
 
     io::stdin().read_line(&mut input_line).unwrap();
-
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if !matches_pattern(&input_line, pattern) {
+        std::process::exit(1);
+    }
 }
